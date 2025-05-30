64a336533772aebab76c20ecc3f36f5db9a27e9c
stag7824 <atalha54@gmail.com>
2025-05-30 23:01:32 +0500

    fix: remove timer from offer banner

--- src/components/OfferBanner.jsx
+++ src/components/OfferBanner.jsx
@@ -54,7 +54,7 @@ const OfferBanner = () => {
           
           <div className="flex items-center space-x-4">
             {/* Countdown Timer */}
-            <div className="flex items-center space-x-1 bg-black/20 px-3 py-1 rounded-full">
+            {/* <div className="flex items-center space-x-1 bg-black/20 px-3 py-1 rounded-full">
               <svg className="w-4 h-4 text-yellow-300" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
               </svg>
@@ -63,7 +63,7 @@ const OfferBanner = () => {
                 {String(timeLeft.minutes).padStart(2, '0')}:
                 {String(timeLeft.seconds).padStart(2, '0')}
               </span>
-            </div>
+            </div> */}
             
             <a 
               href="#packages" 
